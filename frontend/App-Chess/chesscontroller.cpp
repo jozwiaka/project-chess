db62f79f0d493cf105e1ab3d6cffab4164a27a77
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-01-09 15:15:59 +0100

    Update

--- frontend/App-Chess/chesscontroller.cpp
+++ frontend/App-Chess/chesscontroller.cpp
@@ -9,5 +9,4 @@ ChessController::ChessController(ChessModel* model, ChessView* view, QObject *pa
 
     connect(m_View, &ChessView::SquareClicked, m_Model, &ChessModel::UpdateModelOnSquareClick);
     connect(m_Model, &ChessModel::UpdateGraphics, m_View, &ChessView::UpdateChessboardGraphics);
-
 }
