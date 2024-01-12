6ae27750c1889ec936c74ec5d729384ffb6d280c
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-01-12 22:55:34 +0100

    Update

--- frontend/App-Chess/chessgame.cpp
+++ frontend/App-Chess/chessgame.cpp
@@ -7,5 +7,5 @@ ChessGame::ChessGame() : m_Model{Chessboard::GetInstance().Board},
 void ChessGame::Start()
 {
     m_View.show();
-    // m_Model.MoveCNNModel(); //TODO
+    m_Model.MoveCNNModel(); //TODO
 }
