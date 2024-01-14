9d9f33ecf5d1fe9c65acb4c3f05c07cc2d55cc30
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-01-14 14:02:20 +0100

    Update

--- scripts/cnn_model.py
+++ scripts/cnn_model.py
@@ -1,15 +1,32 @@
 import sys
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+# import sklearn.impute
+# import sklearn.compose
+# import sklearn.preprocessing
+# import sklearn.model_selection
+# import tensorflow as tf
+
+# tf.__version__
 
 
 def read_data():
     args = sys.argv
-    if len(args) > 1:
-        return str(args[1])
+    if len(args) <= 1:
+        raise Exception("No data has been received.")
+
+    data = args[1]
+    rows = data.split(";")
+    parsed_data = [row.split(",") for row in rows]
+    return parsed_data
 
 
 def predict_move():
     return "E4"
 
 
-print(read_data())
+# print(read_data())
 print(predict_move())
---
+++ scripts/test.ps1
@@ -0,0 +1,1 @@
+python cnn_model.py "w_rook,w_knight,w_bishop,w_queen,w_king,w_bishop,w_knight,w_rook;w_pawn,w_pawn,w_pawn,w_pawn,w_pawn,w_pawn,w_pawn,w_pawn;e,e,e,e,e,e,e,e;e,e,e,e,e,e,e,e;e,e,e,e,e,e,e,e;e,e,e,e,e,e,e,e;b_pawn,b_pawn,b_pawn,b_pawn,b_pawn,b_pawn,b_pawn,b_pawn;b_rook,b_knight,b_bishop,b_queen,b_king,b_bishop,b_knight,b_rook"
