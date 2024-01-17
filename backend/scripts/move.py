efcaf5a5b3653e725a139486f7882449ae80641e
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-01-17 16:07:42 +0100

    Update

--- backend/scripts/move.py
+++ backend/scripts/move.py
@@ -2,10 +2,17 @@ from classes.chess_model import ChessModel
 import sys
 import paths
 import io
+import chess
+import numpy as np
 
 if __name__ == "__main__":
     chess_model = ChessModel()
     chess_model.load_model(paths.model_path, paths.label_encoder_path)
-    move = chess_model.predict_move(sys.argv[1])
+    fen = sys.argv[1]
+    move = chess_model.predict_move(fen)
+    board = chess.Board(fen)
+    legal_moves = [str(legal_move) for legal_move in board.legal_moves]
+    if move not in legal_moves:
+        move = np.random.choice(legal_moves)
     with open(paths.move_out_path, "w") as f:
         f.write(move)
