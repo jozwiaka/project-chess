df8eaedc87c557ae7e24b2972adda43e3986f836
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-01-17 16:59:22 +0100

    Update

--- backend/scripts/move.py
+++ backend/scripts/move.py
@@ -4,15 +4,23 @@ import paths
 import io
 import chess
 import numpy as np
+from flask import Flask, request, jsonify
 
-if __name__ == "__main__":
-    chess_model = ChessModel()
-    chess_model.load_model(paths.model_path, paths.label_encoder_path)
-    fen = sys.argv[1]
-    move = chess_model.predict_move(fen)
-    board = chess.Board(fen)
+chess_model = ChessModel()
+chess_model.load_model(paths.model_path, paths.label_encoder_path)
+app = Flask(__name__)
+
+
+@app.route("/generate_move", methods=["POST"])
+def generate_move():
+    fen_data = request.json.get("fen_data")
+    move = chess_model.predict_move(fen_data)
+    board = chess.Board(fen_data)
     legal_moves = [str(legal_move) for legal_move in board.legal_moves]
     if move not in legal_moves:
         move = np.random.choice(legal_moves)
-    with open(paths.move_out_path, "w") as f:
-        f.write(move)
+    return jsonify({"move": move})
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
