49e005e66610315eb6bf77fc09b7fd048648c215
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-01-20 10:39:33 +0100

    Update

--- backend/scripts/paths.py
+++ backend/scripts/paths.py
@@ -1,24 +1,13 @@
 import os
 
-web_driver = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))),
-    "web_driver/chromedriver.exe",
-)
-
-plots_dir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "plots"
-)
-
-data_dir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "data"
-)
-
-models_dir = os.path.join(
-    os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "models"
-)
+scripts_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+plots_dir = f"{scripts_dir}/plots"
+data_dir = f"{scripts_dir}/data"
+models_dir = f"{scripts_dir}/models"
+os.makedirs(plots_dir, exist_ok=True)
+os.makedirs(data_dir, exist_ok=True)
+os.makedirs(models_dir, exist_ok=True)
 
 chess_model_path = f"{models_dir}/chess_model.h5"
-
 label_encoder_path = f"{models_dir}/label_encoder.npy"
-
 test_out_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "test.out")
