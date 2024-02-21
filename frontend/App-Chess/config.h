c8bca5ff117e7e6509c48ff362f0abd7a3107f78
jozwiaka <arkadiuszjozwiakdev@gmail.com>
2024-02-21 13:19:57 +0100

    Update

--- frontend/App-Chess/config.h
+++ frontend/App-Chess/config.h
@@ -1,8 +1,9 @@
 #ifndef CONFIG_H
 #define CONFIG_H
-// #define PVP
+
+#define PVP
 // #define PVC
-#define CVC
+// #define CVC
 
 #if !defined(PVP) && !defined(PVC) && !defined(CVC)
 #error "At least one of PVP, PVC, or CVC must be defined."
